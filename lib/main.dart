73cc6b1475fcc1558a0bca6528f528594585667c
Alexander Guevara <alexander.guevaram@hotmail.com>
2021-06-25 15:56:23 -0500

    Primer commit de mi pagina web

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:webprofessional/Routes.dart';
+
+void main() => runApp(MaterialApp(title: "Alexnader Guevara", home: MyApp()));
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Flurorouter.setupRouter();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      onGenerateRoute: Flurorouter.router.generator,
+      // home: Landing(),
+    );
+  }
+}
